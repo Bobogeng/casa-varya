840bbf2ac3efce358f094ef4c0e024872670fe84
Bobogeng <suirito@gmail.com>
2024-02-12 07:27:46 +0700

    Fix textarea max height

--- components/TextAreaField/index.tsx
+++ components/TextAreaField/index.tsx
@@ -15,7 +15,7 @@ export default function TextAreaField({
     <textarea
       className={`${
         full ? "w-full" : "w-fit"
-      } px-4 py-2 rounded-lg bg-black bg-opacity-50 text-2xl text-white placeholder:text-opacity-50 outline-none caret-gray-400 hover:bg-opacity-75 focus:outline-none focus:bg-opacity-100 transition-all ease-out`}
+      } max-h-72 px-4 py-2 rounded-lg bg-black bg-opacity-50 text-2xl text-white placeholder:text-opacity-50 outline-none caret-gray-400 hover:bg-opacity-75 focus:outline-none focus:bg-opacity-100 transition-all ease-out`}
       name={name}
       id={name}
       rows={5}
